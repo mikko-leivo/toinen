b3877004ea866497f05fe594905561a58ea87ed0
MikkoGit <leivo.mikko@gmail.com>
2020-07-06 12:11:01 +0300

    Person luokka päivitetty isosti

--- gittesti.py
+++ gittesti.py
@@ -1,12 +1,32 @@
-class Person():
-    def __init__(self, name ="SirMcEatsAlot", age = 100):
-        self.name = name
-        self.age = age
-    def flexaa(self):
-        x = 1+1
-        print(f'{self.name} miettii hetken ja sanoo 1+1 on {x}')
+class Person:
+    def __init__(self, city, address, phone_number):
+        self._city = city
+        self._address = address
+        self._phone_number = phone_number
 
-mikko = Person("Mikko")
-mervi =Person("Mervi")
+    @property
+    def city(self, _default = None):
+        return self._city
 
-mervi.flexaa()
+    @city.setter
+    def city(self, value):
+        self._city = value
+
+    @property
+    def address(self):
+        return self._address
+
+    @address.setter
+    def address(self, value):
+        self._address = value
+
+    @property
+    def phone_number(self, _default=None):
+        return self._phone_number
+
+    @phone_number.setter
+    def phone_number(self, value):
+        self._phone_number = value
+
+    def __str__(self):
+        return f'Address: {self._address}\nCity: {self._city}\nPhone: {self._phone_number}'
